1b8ebc8d7680497078d94a607b7c86e2666978a7
Christos Karapasias <christos@marteco.nl>
2021-05-05 09:38:26 +0200

    Bounding width

--- MarqueeControl.xaml.cs
+++ MarqueeControl.xaml.cs
@@ -8,10 +8,8 @@ namespace UWPMarquee
     public sealed partial class MarqueeControl : UserControl
     {
         public static readonly DependencyProperty SpacingProperty = DependencyProperty.Register("Spacing", typeof(double), typeof(MarqueeControl), new PropertyMetadata(default(double)));
-        public static readonly DependencyProperty SecondProperty = 
-            DependencyProperty.Register("Second", typeof(FrameworkElement), typeof(MarqueeControl), new PropertyMetadata(default(FrameworkElement)));
-        public static readonly DependencyProperty FirstProperty = 
-            DependencyProperty.Register("First", typeof(FrameworkElement), typeof(MarqueeControl), new PropertyMetadata(default(FrameworkElement)));
+        public static readonly DependencyProperty SecondProperty = DependencyProperty.Register("Second", typeof(TextBlock), typeof(MarqueeControl), new PropertyMetadata(default(TextBlock)));
+        public static readonly DependencyProperty FirstProperty = DependencyProperty.Register("First", typeof(TextBlock), typeof(MarqueeControl), new PropertyMetadata(default(TextBlock)));
         public MarqueeControl()
         {
             this.InitializeComponent();
@@ -23,15 +21,15 @@ namespace UWPMarquee
             set { SetValue(SpacingProperty, value); }
         }
 
-        public FrameworkElement Second
+        public TextBlock Second
         {
-            get { return (FrameworkElement)GetValue(SecondProperty); }
+            get { return (TextBlock)GetValue(SecondProperty); }
             set { SetValue(SecondProperty, value); }
         }
 
-        public FrameworkElement First
+        public TextBlock First
         {
-            get { return (FrameworkElement)GetValue(FirstProperty); }
+            get { return (TextBlock)GetValue(FirstProperty); }
             set { SetValue(FirstProperty, value); }
         }
 
@@ -43,7 +41,7 @@ namespace UWPMarquee
 
             SbFr2.From = 0;
             SbFr2.To = -txtKron2.Margin.Left;
-            if (txtKron.ActualWidth >= e.NewSize.Width)
+            if (txtKron.ActualWidth >= Grd.ActualWidth)
             {
                 slide.Begin();
                 txtKron2.Visibility = Visibility.Visible;
